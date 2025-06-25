96d7678d2112606a4cc1761eb906022b8222a179
popspeed1st <i.hate.u.19xx@gmail.com>
2025-06-25 19:46:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 import os
 from dotenv import load_dotenv
-import asyncio
 
 load_dotenv(dotenv_path="env")
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
     await update.message.reply_text("บอททำงานแล้วจ้า! 🚀")
 
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
